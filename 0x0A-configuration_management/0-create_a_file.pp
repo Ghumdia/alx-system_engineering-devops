2bee0795d47a9c9692d4ba3e60c9111e6ad76a8b
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-01-11 15:53:10 +0100

    Committed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,10 @@
-#creates a file in /tmp
+# Create a file in /tmp
 
-file { '/tmp/school':
-
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
