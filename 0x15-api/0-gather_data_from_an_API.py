5a2be610062e1608286eb34eff1aaf47fbb0e1cc
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-03-14 18:17:36 +0100

    Committed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,12 +4,22 @@ Returns the to-do lists of an employee
 """
 import requests
 import sys
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        user_url = 'https://jsonplaceholder.typicode.com/users/'
+        todo_url = 'https://jsonplaceholder.typicode.com/todos?userId='
+
+        NAME = requests.get(user_url + sys.argv[1]).json()['name']
+        DONE_TASKS = len([task for task in requests.
+                                    get(todo_url + sys.argv[1]).json()
+                                    if task['completed'] is True])
+        TASKS = len(requests.get(todo_url + sys.argv[1]).json())
+        DONE_TASKS_TITLES = [task['title'] for task in requests.
+                             get(todo_url + sys.argv[1]).json()
+                             if task['completed'] is True]
+
+        print('Employee {} is done with tasks({}/{}):'.
+              format(NAME, DONE_TASKS,
+                     TASKS))
+        for TASK_TITLE in DONE_TASKS_TITLES:
+            print('\t {}'.format(TASK_TITLE))
