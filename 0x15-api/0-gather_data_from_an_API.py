587f63423af75621fae636796fda532f1a67b929
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-03-14 18:13:08 +0100

    Committed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,16 +4,12 @@ Returns the to-do lists of an employee
 """
 import requests
 import sys
-"""
-Imports the necessary modules
-"""
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-url = "https://jsonplaceholder.typicode.com/"
-id = int(sys.argv[1])
-user = requests.get(url + "users/{}".format(id)).json()
-todos = requests.get(url + "todos", params={"userId": id}).json()
-
-completed = [t.get("title") for t in todos if t.get("completed") is True]
-print("Employee {} is done with tasks({}/{}):".format(
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-[print("\t {}".format(c)) for c in completed]
+    [print("\t {}".format(c)) for c in completed]
