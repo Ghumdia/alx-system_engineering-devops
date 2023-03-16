1f4d8476d96014bdccd5be883b8f7890b58bb070
John Billy <bosass80@gmail.com>
2023-03-16 13:53:12 -0700

    Committed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,20 +3,32 @@
 
 import csv
 import requests
-from sys import argv
-
+import sys
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user_id = argv[1]
-    user = requests.get(url + 'users/{}'.format(user_id)).json()
-    tasks = requests.get(url + 'todos', params={'userId': user_id}).json()
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        user_id = sys.argv[1]
+        user_res = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+        todos_res = requests.get('https://jsonplaceholder.typicode.com/todos', params={'userId': user_id})
 
-    # Save data to CSV file
-    with open('{}.csv'.format(user_id), mode='w', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',', quotechar='"',
-                            quoting=csv.QUOTE_ALL)
-        for task in tasks:
-            writer.writerow([user_id, user['username'], task['completed'],
-                             task['title']])
+        try:
+            username = user_res.json()['username']
+            todos = todos_res.json()
+        except ValueError:
+            print('Invalid JSON')
+            sys.exit(1)
+
+        with open('{}.csv'.format(user_id), mode='w', newline='') as csv_file:
+            fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for todo in todos:
+                writer.writerow({
+                    'USER_ID': user_id,
+                    'USERNAME': username,
+                    'TASK_COMPLETED_STATUS': str(todo['completed']),
+                    'TASK_TITLE': todo['title']
+                })
+    else:
+        print('Usage: {} USER_ID'.format(sys.argv[0]))
 
