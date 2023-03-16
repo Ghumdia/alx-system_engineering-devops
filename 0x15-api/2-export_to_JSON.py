d376a732616a7313794fedd662c7b4b2d21bffa6
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-03-16 23:04:52 +0100

    Committed

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
 """
-Returns to-do list information for a given employee ID.
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
 """
+if __name__ == "__main__":
 
-import json
-import requests
-import sys
+    import requests
+    import sys
+    import json
 
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        u_url = 'https://jsonplaceholder.typicode.com/users/'
+        td_url = 'https://jsonplaceholder.typicode.com/todos?userId='
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+        USER_ID = requests.get(u_url + sys.argv[1]).json()['id']
+        USERNAME = requests.get(u_url + sys.argv[1]).json()['username']
+        TASK_COMPLETED_STATUSES = [task['completed'] for task in
+                                   requests.get(td_url + sys.argv[1]).json()]
+        TASKS_TITLES = [task['title'] for task in
+                        requests.get(td_url + sys.argv[1]).json()]
+        TOTAL_NUMBER_OF_TASKS = len(requests.get(td_url + sys.argv[1]).json())
 
-    id = sys.argv[1]
+        TASKS_DICT = {"{}".format(USER_ID):
+                      [{"task": "{}".format(TASKS_TITLES[N]),
+                        "completed": TASK_COMPLETED_STATUSES[N],
+                        "username": "{}".format(USERNAME)}
+                       for N in range(TOTAL_NUMBER_OF_TASKS)]}
 
-    response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(id))
-    todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
-
-    employee_name = response.json()['name']
-
-    tasks = []
-    for task in todos.json():
-        task_dict = {
-            "task": task['title'],
-            "completed": task['completed'],
-            "username": employee_name
-        }
-        tasks.append(task_dict)
-
-    json_data = {id: tasks}
-    with open("{}.json".format(id), "w") as json_file:
-        json.dump(json_data, json_file)
+        with open('{}.json'.format(USER_ID), 'w') as fp:
+            json.dump(TASKS_DICT, fp)
