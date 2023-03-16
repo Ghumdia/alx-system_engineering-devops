93e5c5490b1b4eefed27b3decede13df95361735
John Billy <bosass80@gmail.com>
2023-03-16 14:19:38 -0700

    Committed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,21 +19,17 @@ if __name__ == '__main__':
 
     # Extract the necessary information from the API response
     employee_name = response.json()['name']
+    tasks = len(todos.json())
+    d_tasks = len([todo for todo in todos.json() if todo['completed']])
 
-    # Create a CSV file and write header row
     filename = '{}.csv'.format(id)
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
-
         for todo in todos.json():
+
             # Extract information from todo item
             task_completed = str(todo['completed'])
             task_title = todo['title']
             
             # Write information to CSV file
             writer.writerow([id, employee_name, task_completed, task_title])
-    
-    # Display success message
-    print('Data successfully exported to {}'.format(filename))
-
