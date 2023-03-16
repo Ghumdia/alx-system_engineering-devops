a5be1633cda62d0f38c09bed081e57820c57b135
John Billy <bosass80@gmail.com>
2023-03-16 14:59:11 -0700

    Committed

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,46 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+"""Returns to-do list information for all employees."""
+import csv
 import json
 import requests
+import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
+if __name__ == '__main__':
+    # Retrieve all employees from the API
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users')
+    employees = response.json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    # Create a dictionary to store the employee tasks
+    employee_tasks = {}
+    for employee in employees:
+        # Retrieve the TODO list for the employee
+        id = employee['id']
+        todos = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
+        
+        # Extract the necessary information from the API response
+        employee_name = employee['name']
+        tasks = []
+        for todo in todos.json():
+            # Extract information from todo item
+            task_completed = todo['completed']
+            task_title = todo['title']
+            
+            # Add information to tasks list
+            tasks.append({
+                'username': employee_name,
+                'task': task_title,
+                'completed': task_completed
+            })
+        
+        # Add tasks to employee_tasks dictionary
+        employee_tasks[str(id)] = tasks
+    
+    # Export the data to a JSON file
+    filename = 'todo_all_employees.json'
+    with open(filename, 'w') as file:
+        json.dump(employee_tasks, file)
+    
+    # Display success message
+    print('Data successfully exported to {}'.format(filename))
