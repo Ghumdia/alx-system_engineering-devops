54f865b71a995f204b5240903aece8fe15a0ca8b
John Billy <bosass80@gmail.com>
2023-03-16 14:04:58 -0700

    Committed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""Script that exports data in the CSV format."""
-
-import csv
+"""Exports to-do list information for a given employee ID in CSV format."""
 import requests
 import sys
+import csv
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        user_id = sys.argv[1]
-        user_res = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
-        todos_res = requests.get('https://jsonplaceholder.typicode.com/todos', params={'userId': user_id})
+    # Check if the employee ID is provided as a command-line argument
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
 
-        try:
-            username = user_res.json()['username']
-            todos = todos_res.json()
-        except ValueError:
-            print('Invalid JSON')
-            sys.exit(1)
+    # Retrieve the employee information and TODO list from the API
+    id = sys.argv[1]
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(id))
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
 
-        with open('{}.csv'.format(user_id), mode='w', newline='') as csv_file:
-            fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            writer.writeheader()
-            for todo in todos:
-                writer.writerow({
-                    'USER_ID': user_id,
-                    'USERNAME': username,
-                    'TASK_COMPLETED_STATUS': str(todo['completed']),
-                    'TASK_TITLE': todo['title']
-                })
-    else:
-        print('Usage: {} USER_ID'.format(sys.argv[0]))
+    # Extract the necessary information from the API response
+    employee_name = response.json()['name']
+    tasks = len(todos.json())
+    d_tasks = len([todo for todo in todos.json() if todo['completed']])
+    task_list = [[id, employee_name, todo['completed'], todo['title']] for todo in todos.json()]
+
+    # Save the employee TODO list progress in a CSV file
+    with open('{}.csv'.format(id), mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(task_list)
+    print("Data exported successfully to {}.csv".format(id))
 
