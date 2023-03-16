212b27984904b0b3fe5c85c7ac8ad3d12c3cbd49
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-03-16 23:05:13 +0100

    Committed

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,46 +1,37 @@
 #!/usr/bin/python3
-"""Returns to-do list information for all employees."""
-import csv
-import json
-import requests
-import sys
+"""
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    # Retrieve all employees from the API
-    response = requests.get(
-        'https://jsonplaceholder.typicode.com/users')
-    employees = response.json()
+    import requests
+    import sys
+    import json
 
-    # Create a dictionary to store the employee tasks
-    employee_tasks = {}
-    for employee in employees:
-        # Retrieve the TODO list for the employee
-        id = employee['id']
-        todos = requests.get(
-            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
-        
-        # Extract the necessary information from the API response
-        employee_name = employee['name']
-        tasks = []
-        for todo in todos.json():
-            # Extract information from todo item
-            task_completed = todo['completed']
-            task_title = todo['title']
-            
-            # Add information to tasks list
-            tasks.append({
-                'username': employee_name,
-                'task': task_title,
-                'completed': task_completed
-            })
-        
-        # Add tasks to employee_tasks dictionary
-        employee_tasks[str(id)] = tasks
-    
-    # Export the data to a JSON file
-    filename = 'todo_all_employees.json'
-    with open(filename, 'w') as file:
-        json.dump(employee_tasks, file)
-    
-    # Display success message
-    print('Data successfully exported to {}'.format(filename))
+    if len(sys.argv) == 1:
+        u_url = 'https://jsonplaceholder.typicode.com/users/'
+        td_url = 'https://jsonplaceholder.typicode.com/todos?userId='
+
+        ALL_USER_IDS = [user['id'] for user in requests.get(u_url).json()]
+
+        TASKS_DICT_ALL_USERS = {}
+        for USER_ID in ALL_USER_IDS:
+            USERNAME = requests.get(u_url + str(USER_ID)).json()['username']
+            TASK_COMPLETED_STATUSES = [task['completed'] for task in
+                                       requests.get(td_url + str(USER_ID))
+                                       .json()]
+            TASKS_TITLES = [task['title'] for task in
+                            requests.get(td_url + str(USER_ID)).json()]
+            TOTAL_NUMBER_OF_TASKS = len(requests.get(td_url + str(USER_ID))
+                                        .json())
+            TASKS_DICT = {"{}".format(USER_ID):
+                          [{"task": "{}".format(TASKS_TITLES[N]),
+                            "completed": TASK_COMPLETED_STATUSES[N],
+                            "username": "{}".format(USERNAME)}
+                           for N in range(TOTAL_NUMBER_OF_TASKS)]}
+            TASKS_DICT_ALL_USERS["{}".format(USER_ID)] = TASKS_DICT[
+                "{}".format(USER_ID)]
+
+        with open('todo_all_employees.json', 'w') as fp:
+            json.dump(TASKS_DICT_ALL_USERS, fp)
