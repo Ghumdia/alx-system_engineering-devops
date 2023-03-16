25b0982718c2dba4f8d8bcce0fecd9fc1a39a079
John Billy <bosass80@gmail.com>
2023-03-16 14:37:29 -0700

    Committed

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,43 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
-import json
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
+import json
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+if __name__ == '__main__':
+    # Check if the employee ID is provided as a command-line argument
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    # Retrieve the employee information and TODO list from the API
+    id = sys.argv[1]
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(id))
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
+
+    # Extract the necessary information from the API response
+    employee_name = response.json()['name']
+    tasks = todos.json()
+    tasks_list = []
+    for task in tasks:
+        task_dict = {
+            'task': task['title'],
+            'completed': task['completed'],
+            'username': employee_name
+        }
+        tasks_list.append(task_dict)
+
+    # Export the employee TODO list in JSON format
+    filename = '{}.json'.format(id)
+    with open(filename, 'w') as f:
+        json.dump({id: tasks_list}, f, indent=4)
+
+    # Display the employee TODO list progress in the required format
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len([task for task in tasks if task['completed']]), len(tasks)))
+    for task in tasks:
+        if task['completed']:
+            print("\t{}".format(task['title']))
+
