76994925e67dcc7acfcbda0171a82066cd74950a
John Billy <bosass80@gmail.com>
2023-03-16 13:30:28 -0700

    Committed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,8 +11,10 @@ if __name__ == '__main__':
 
     # Retrieve the employee information and TODO list from the API
     employee_id = sys.argv[1]
-    response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
 
     # Extract the necessary information from the API response
     employee_name = response.json()['name']
@@ -20,7 +22,8 @@ if __name__ == '__main__':
     done_tasks = len([todo for todo in todos.json() if todo['completed']])
 
     # Display the employee TODO list progress in the required format
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_tasks, total_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, done_tasks, total_tasks))
     for todo in todos.json():
         if todo['completed']:
             print("\t {}".format(todo['title']))
