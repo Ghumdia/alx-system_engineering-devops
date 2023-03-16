47e3e60d9c299fafaec6507b1e94a40ba4a5becd
John Billy <bosass80@gmail.com>
2023-03-16 13:26:41 -0700

    Committed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,12 +3,24 @@
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+if __name__ == '__main__':
+    # Check if the employee ID is provided as a command-line argument
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    # Retrieve the employee information and TODO list from the API
+    employee_id = sys.argv[1]
+    response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
+
+    # Extract the necessary information from the API response
+    employee_name = response.json()['name']
+    total_tasks = len(todos.json())
+    done_tasks = len([todo for todo in todos.json() if todo['completed']])
+
+    # Display the employee TODO list progress in the required format
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_tasks, total_tasks))
+    for todo in todos.json():
+        if todo['completed']:
+            print("\t {}".format(todo['title']))
