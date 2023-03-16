571792d12c12c2d422d0681550b664dc4ceef491
John Billy <bosass80@gmail.com>
2023-03-16 14:52:54 -0700

    Committed

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,30 +7,31 @@ import json
 import requests
 import sys
 
+
 if __name__ == '__main__':
-    # Check if the employee ID is provided as a command-line argument
     if len(sys.argv) != 2:
         print("Usage: python3 gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    # Retrieve the employee information and TODO list from the API
     id = sys.argv[1]
-    response = requests.get('https://jsonplaceholder.typicode.com/users/{}'\
-		.format(id))
-    todos = requests.get('https://jsonplaceholder.typicode.com/\
-			todos?userId={}'.format(id))
 
-    # Extract the necessary information from the API response
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(id))
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
+
     employee_name = response.json()['name']
-    task_list = []
-    for todo in todos.json():
-        task_list.append({
-            "task": todo['title'],
-            "completed": todo['completed'],
-            "username": employee_name
-        })
 
-    # Write the task list to a JSON file
-    filename = '{}.json'.format(id)
-    with open(filename, mode='w') as file:
-        json.dump({id: task_list}, file)
+    tasks = []
+    for task in todos.json():
+        task_dict = {
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": employee_name
+        }
+        tasks.append(task_dict)
+
+    json_data = {id: tasks}
+    with open("{}.json".format(id), "w") as json_file:
+        json.dump(json_data, json_file)
+
