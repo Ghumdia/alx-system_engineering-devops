64020ca94b57abfb847488d770af4b9f321cf019
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-05-09 15:42:20 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,9 @@
-"""Importing required modules"""
-from pip._vendor import requests
+#!/usr/bin/python3
+"""Makes a query for the subscriber counts for a sub reddit"""
+import requests
 
 def number_of_subscribers(subreddit):
+    """Returns the number of subscriber counts for a subreddit"""
     x = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers={'User-Agent': 'Mozilla/5.0'})
     if x.ok:
         data = x.json()
