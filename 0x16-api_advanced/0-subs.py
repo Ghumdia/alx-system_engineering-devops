16c3f51b3bc067898f2c1ef9a1e1b1ba66a0fd56
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-05-09 15:36:25 +0100

    committed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,14 @@
-#!/usr/bin/python3
-
 """Importing required modules"""
 from pip._vendor import requests
 
 def number_of_subscribers(subreddit):
-    x = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers={'User-Agent': 'Mozilla/5.0'}
-:Wq
-
+    x = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers={'User-Agent': 'Mozilla/5.0'})
+    if x.ok:
+        data = x.json()
+        try:
+            sub_count = data['data']['subscribers']
+            print(sub_count)
+        except KeyError:
+            print(0)
+    else :
+        print(0)
